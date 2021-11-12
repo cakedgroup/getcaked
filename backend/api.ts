8279737f9072252d16858fb65dc952ddea8f0f21
Jannik Springer <jannik.springer.30.40@gmail.com>
2021-11-12 17:47:39 +0100

    Created Database and Backend Connection to db

--- backend/api.ts
+++ backend/api.ts
@@ -1,14 +1,14 @@
 import express from 'express'
-import { echoController } from './controllers/echo';
+import * as db from './databaseAccess/db';
 import * as logger from './util/logger';
 import cors from 'cors';
 const router = express.Router();
 
+console.log(db);
+
 router.use(cors());
 router.use(express.json());
 router.use(logger.logToConsole);
-
-router.use('/echo', echoController);
 
 router.use((_req: express.Request, res: express.Response) => {
   res.status(404);
---
+++ backend/databaseAccess/db.ts
@@ -0,0 +1,46 @@
+import sqlite from 'sqlite3';
+
+export const db = new sqlite.Database('/home/jannik/Projects/repos/getcaked/backend/databases/getcaked.sqlite', (err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log('Established connection to database');
+        createTables();
+    }
+})
+
+function createTables() : void {
+    db.run(`PRAGMA foreign_keys = ON`, (err) => {
+        if (err) {
+            console.log(err)
+        }
+    })
+    db.run(`CREATE TABLE IF NOT EXISTS users(userId TEXT PRIMARY KEY NOT NULL, username TEXT NOT NULL, 
+            passwordHash TEXT NOT NULL)`, (err) => {
+        if (err) {
+            console.log("1. Run" + err);
+        }
+    });
+    db.run(`CREATE TABLE IF NOT EXISTS groups(groupId TEXT PRIMARY KEY NOT NULL, groupName TEXT NOT NULL, 
+            type TEXT NOT NULL, adminId TEXT NOT NULL, FOREIGN KEY (adminId) REFERENCES users(userId))`, (err) => {
+        if (err) {
+            console.log("2. Run" + err);
+        }
+    });
+    db.run(`CREATE TABLE IF NOT EXISTS cakeEvents(cakeId TEXT PRIMARY KEY NOT NULL, timestamp DATE NOT NULL, 
+            username TEXT NOT NULL, cakeDelivered INTEGER NOT NULL, userId TEXT, 
+            groupId TEXT NOT NULL, FOREIGN KEY (userId) REFERENCES users(userId),
+            FOREIGN KEY (groupId) REFERENCES groups(groupId))`, (err) => {
+        if (err) {
+            console.log("3. Run" + err);
+        }
+    });
+    db.run(`CREATE TABLE IF NOT EXISTS members(userId TEXT NOT NULL, groupId TEXT NOT NULL, 
+            FOREIGN KEY (userId) REFERENCES users(userId), 
+            FOREIGN KEY (groupId) REFERENCES groups(groupId), PRIMARY KEY (userId, groupId))`, (err) => {
+        if (err) {
+            console.log("4. Run" + err);
+        }
+    });
+}
