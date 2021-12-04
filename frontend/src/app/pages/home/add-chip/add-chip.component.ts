862a292dedba38a639c794bec34d7bd55f171557
grossamos <email@amosgross.com>
2021-12-04 12:09:49 +0100

    created group add chip

---
+++ frontend/src/app/pages/home/add-chip/add-chip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-chip',
+  templateUrl: './add-chip.component.html',
+  styleUrls: ['./add-chip.component.css']
+})
+export class AddChipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
