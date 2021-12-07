0c97050300912d6a2398f2b453342262b388fcf4
Amos Groß <email@amosgross.com>
2021-12-07 19:15:07 +0100

    Added back to buttons (#43)
    * Added back too buttons
    - in admin pannel
    - in success view
    - in failed view
    
    * fixed minor issues for PR
    
    * fixed 2 links
    
    Co-authored-by: Jannik Springer <jannik.springer.30.40@gmail.com>

--- frontend/src/app/pages/groups/cake-failure/cake-failure.component.ts
+++ frontend/src/app/pages/groups/cake-failure/cake-failure.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cake-failure',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CakeFailureComponent implements OnInit {
 
-  constructor() { }
+  groupId: string;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.groupId = this.route.snapshot.params['groupId'];
   }
 
 }
--- frontend/src/app/pages/groups/cake-success/cake-success.component.ts
+++ frontend/src/app/pages/groups/cake-success/cake-success.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cake-success',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CakeSuccessComponent implements OnInit {
 
-  constructor() { }
+  groupId: string;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.groupId = this.route.snapshot.params['groupId'];
   }
 
 }
