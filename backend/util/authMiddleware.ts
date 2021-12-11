58518c5e18709f8bd2aef96a3ee0211f49dc7755
Amos Groß <email@amosgross.com>
2021-12-11 18:08:30 +0100

    fixed potential security vulnerabilities (#75)
    * added check for deleted users in authMiddleWare
    
    * added missing algorithm
    
    * changed status code
    
    Co-authored-by: Jannik Springer <jannik.springer.30.40@gmail.com>

--- backend/util/authMiddleware.ts
+++ backend/util/authMiddleware.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import {User} from '../models/User';
+import { getUserInfo } from '../services/userService';
 
 /**
  * Decode the JWT-Token given in the Authorization header, while verifying its integrity
@@ -12,11 +13,21 @@ export function getUserAuth(req: express.Request, res: express.Response, next: e
 	const token: string | undefined = req.header('Authorization');
 	if (token) {
 		try {
-			req.decoded = jwt.verify(token, process.env.JWT_SECRET as string) as User;
+			req.decoded = jwt.verify(token, process.env.JWT_SECRET as string, {algorithms: ['HS256']}) as User;
+			// check if user actually exists
+			getUserInfo(req.decoded.userId).then(() => {
+				next();
+			}).catch(() => {
+				res.status(403);
+				res.send();
+			} );
 		}
 		catch (err) {
 			req.decoded = undefined; // jwt couldn't be decoded -> handle, as if none was given
+			next();
 		}
+	} 
+	else {
+		next();
 	}
-	next();
 }
