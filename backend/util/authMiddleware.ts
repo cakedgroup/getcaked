51e8adbd22e3d06d7a7f921dd5529d6ac9747ddf
grossamos <email@amosgross.com>
2021-12-11 12:12:16 +0100

    added check for deleted users in authMiddleWare

--- backend/util/authMiddleware.ts
+++ backend/util/authMiddleware.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import {User} from '../models/User';
+import { getUserInfo } from '../services/userService';
 
 /**
  * Decode the JWT-Token given in the Authorization header, while verifying its integrity
@@ -13,10 +14,18 @@ export function getUserAuth(req: express.Request, res: express.Response, next: e
 	if (token) {
 		try {
 			req.decoded = jwt.verify(token, process.env.JWT_SECRET as string) as User;
+			// check if user actually exists
+			getUserInfo(req.decoded.userId).then((user: User) => {
+				console.log(user);
+				next();
+			}).catch(() => {
+				res.status(401);
+				res.send();
+			} );
 		}
 		catch (err) {
 			req.decoded = undefined; // jwt couldn't be decoded -> handle, as if none was given
+			next();
 		}
 	}
-	next();
 }
